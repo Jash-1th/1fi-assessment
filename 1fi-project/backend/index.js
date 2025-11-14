0828361869abed6a16d372996ec8775c84338452
Jash-1th <jaswanthmamilla@gmail.com>
2025-11-14 12:56:00 +0530

    cors added

--- 1fi-project/backend/index.js
+++ 1fi-project/backend/index.js
@@ -2,12 +2,32 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import productRoutes from './routes/productRoutes.js'; // Import routes
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+
+const allowedOrigins = [
+  'https://if1-assessment.netlify.app',
+  'http://localhost:5173' 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+
+app.use(cors(corsOptions));
+// ----------------------------------------
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -26,10 +46,8 @@ mongoose
     process.exit(1);
   });
 
-// --- API Routes ---
-app.use('/api/products', productRoutes); // Use the product routes
+app.use('/api/products', productRoutes);
 
-// Test route
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend! 👋' });
 });
